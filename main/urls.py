3e7094c425871c46960d0efe9e2db93b605907ce
AnggaPutraa <yananggaa@gmail.com>
2022-11-26 20:26:28 +0700

    refactor: register login, register, & logout endpoints

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
+    path('flutter-login-user/', flutter_login_user, name='flutter-login-user'),
+    path('flutter-register-user', flutter_register_user, name='flutter-register-user'),
+    path('flutter-logout-user', flutter_logout_user, name='flutter-logout-user'),
 ]
