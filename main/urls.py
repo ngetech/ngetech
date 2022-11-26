8634c6e8bbf85740ea1d586069b60671e5e43f8c
AnggaPutraa <yananggaa@gmail.com>
2022-11-26 20:26:58 +0700

    refactor: fix typo

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
     path('flutter-login-user/', flutter_login_user, name='flutter-login-user'),
-    path('flutter-register-user', flutter_register_user, name='flutter-register-user'),
-    path('flutter-logout-user', flutter_logout_user, name='flutter-logout-user'),
+    path('flutter-register-user/', flutter_register_user, name='flutter-register-user'),
+    path('flutter-logout-user/', flutter_logout_user, name='flutter-logout-user'),
 ]
