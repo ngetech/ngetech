5b6e867a7611a67895180022ad0f29de692dc1d9
AnggaPutraa <yananggaa@gmail.com>
2022-12-02 09:50:40 +0700

    refactor: test lag fixing

--- main/static/js/navbar.js
+++ main/static/js/navbar.js
@@ -1,17 +1,21 @@
 const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('.menu');
-hamburger.addEventListener(
-    'click',
-    function () {
-        menu.classList.toggle('hidden');
-    }
-)
+if (hamburger != null) {
+    hamburger.addEventListener(
+        'click',
+        function () {
+            menu.classList.toggle('hidden');
+        }
+    )
+}
 
 const avatar = document.querySelector('#avatarButton');
-const avatarMenu = document.querySelector('#userDropdown');
-avatar.addEventListener(
-    'click',
-    function () {
-        avatarMenu.classList.toggle('hidden');
-    }
-)
+if (avatar != null) {
+    const avatarMenu = document.querySelector('#userDropdown');
+    avatar.addEventListener(
+        'click',
+        function () {
+            avatarMenu.classList.toggle('hidden');
+        }
+    )
+}
